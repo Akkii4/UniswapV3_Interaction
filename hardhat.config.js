e48c6bf1ba48d105930e09e62a0c719b0862b81b
Akshit Choudhary <akshitchoudhary007@gmail.com>
2023-05-16 16:55:11 +0530

    fixed network url

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
   networks: {
     hardhat: {
       forking: {
-        url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.PRIVATE_KEY}`,
+        url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
       },
     },
   },
