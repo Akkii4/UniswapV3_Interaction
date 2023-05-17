0f2d3afa8ffbdc3516b377b5f1b550b8dbca25e9
Akshit Choudhary <akshitchoudhary007@gmail.com>
2023-05-17 13:42:14 +0530

    updated solidity compiler config
    For fixing Stack too deep while compiling

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,20 @@ require("dotenv").config();
 require("solidity-docgen");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.7.6",
+  solidity: {
+    compilers: [
+      {
+        version: "0.7.6",
+        settings: {
+          evmVersion: "istanbul",
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
+    ],
+  },
   networks: {
     hardhat: {
       forking: {
